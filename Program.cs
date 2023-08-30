ad6b29c27695b7572a77e6a8f6883d6f60869ec3
Vasya Pupkin <vasya.pupkin@email.com>
2023-08-30 21:59:48 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
-﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9*/
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+позиция i = 4, j = 2 -> такой позиции нет
+позиция i = 1, j = 0 -> 5*/
 
 Console.WriteLine("Введите количество элементов двумерного массива m:");
-int m = int.Parse(Console.ReadLine()!);
+int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество элементов двумерного массива n:");
-int n = int.Parse(Console.ReadLine()!);
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер элемента массива x:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер элемента массива y:");
+int y = Convert.ToInt32(Console.ReadLine());;
 
 double [,] matrix =GetArray(m,n);
 PrintArray(matrix);
+if (x>matrix.GetLength (0) || y>matrix.GetLength (1))
+{
+Console.WriteLine("Такой позиции нет");
+}
+else 
+Console.Write($"({matrix[x,y]})");
+{
+
+}
 
 double [,] GetArray(int m, int n)
 {
@@ -19,7 +35,7 @@ double [,] GetArray(int m, int n)
     {
         for (int j=0;j<matrix.GetLength (1);j++)
         {
-           matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+           matrix[i, j] = new Random().Next(-10, 10);
             
         }
     
