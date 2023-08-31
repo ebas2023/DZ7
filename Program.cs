9a7b48690a926eded2a28bb68b97ad55da4d96cd
ebas2023 <139645893+ebas2023@users.noreply.github.com>
2023-08-31 19:35:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,50 @@
-﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9*/
+/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3*/
+
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 
 Console.WriteLine("Введите количество элементов двумерного массива m:");
 int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите количество элементов двумерного массива n:");
 int n = int.Parse(Console.ReadLine()!);
 
-double [,] matrix =GetArray(m,n);
+int [,] matrix =GetArray(m,n);
 PrintArray(matrix);
 
-double [,] GetArray(int m, int n)
+for (int j = 0; j < matrix.GetLength(1); j++)
 {
-    double [,] matrix=new double[m,n];
+    double sumsrzn= 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sumsrzn =(sumsrzn + matrix[i, j]);
+    }
+    sumsrzn = sumsrzn / n;
+
+    Console.Write($"{Math.Round(sumsrzn,2)} ");
+}
+
+
+int [,] GetArray(int m, int n)
+{
+    int [,] matrix=new int[m,n];
     for (int i=0;i<matrix.GetLength (0);i++)
     {
         for (int j=0;j<matrix.GetLength (1);j++)
         {
-           matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
-            
-        }
+           matrix[i, j] = new Random().Next(0, 10);
+                   }
     
     }
     return matrix;
 }
 
-void PrintArray (double[,] matrix)
+void PrintArray (int[,] matrix)
 {
     for (int i=0;i<matrix.GetLength (0);i++)
     {
